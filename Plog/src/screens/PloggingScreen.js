b31da25062741696b466050acea5fa1fe066df5b
gawon <gawon_127@naver.com>
2024-09-25 14:03:42 +0900

    [FEAT] 플로깅화면 타이머 코드

--- Plog/src/screens/PloggingScreen.js
+++ Plog/src/screens/PloggingScreen.js
@@ -29,7 +29,6 @@ const PloggingScreen = ({navigation}) => {
   // 시간을 "00:00" 형식으로 변환하는 함수
   const formatTime = (secs) => {
     const time = parseInt(secs / 60) + ':' + parseInt(secs % 60)
-
     return time.replace(/\b(\d)\b/g, '0$1');
   };
 
