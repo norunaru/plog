f23b48ff821094c7eb126b4f5a803285c1d04e07
이재준 <sktks11@gmail.com>
2024-10-04 10:51:11 +0900

    #20 - 유저 평점 반영 추가

--- Plog/src/main/java/com/plog/backend/domain/activity/service/ActivityServiceImpl.java
+++ Plog/src/main/java/com/plog/backend/domain/activity/service/ActivityServiceImpl.java
@@ -11,7 +11,11 @@ import com.plog.backend.domain.activity.entity.ActivityImage;
 import com.plog.backend.domain.activity.repository.ActivityImageRepository;
 import com.plog.backend.domain.activity.repository.ActivityRepository;
 import com.plog.backend.domain.member.entity.Member;
+import com.plog.backend.domain.member.entity.MemberScore;
 import com.plog.backend.domain.member.repository.MemberRepository;
+import com.plog.backend.domain.member.repository.MemberScoreRepository;
+import com.plog.backend.domain.trail.entity.Trail;
+import com.plog.backend.domain.trail.repository.TrailRepository;
 import com.plog.backend.global.s3.service.S3Service;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -32,6 +36,7 @@ public class ActivityServiceImpl implements ActivityService {
 
     private final ActivityRepository activityRepository;
     private final ActivityImageRepository activityImageRepository;
+    private final MemberScoreRepository memberScoreRepository;
     private final MemberRepository memberRepository;
     private final ModelMapper mapper;
     private final S3Service s3Service;
@@ -55,7 +60,15 @@ public class ActivityServiceImpl implements ActivityService {
         Member member = memberRepository.findById(memberId)
             .orElseThrow(() -> new IllegalArgumentException("Invalid member ID: " + memberId));
 
-        // 2. Activity 엔티티를 생성하여 저장
+        // 2. 클러스터링을 위한 유저 점수 추가
+        // 2.1. trailId로 Trail 조회
+        long trailId = activity.getTrailId();
+        // 2.2. trailId
+        MemberScore memberScore = memberScoreRepository.findByMemberId(memberId);
+        memberScore.getScore()[(int)trailId] = activity.getScore();
+        memberScoreRepository.save(memberScore);
+
+        // 3. Activity 엔티티를 생성하여 저장
         Activity newActivity = Activity.builder()
             .member(member)
             .title(activity.getTitle())
@@ -127,6 +140,14 @@ public class ActivityServiceImpl implements ActivityService {
 
         // 3. Activity 엔티티 업데이트
         existingActivity.update(activityDto, imageUrls);
+
+        // 3. 클러스터링을 위한 유저 점수 추가
+        // 3.1. trailId로 Trail 조회
+        long trailId = existingActivity.getTrail().getId();
+        // 3.2. trailId
+        MemberScore memberScore = memberScoreRepository.findByMemberId(memberId);
+        memberScore.getScore()[(int)trailId] = existingActivity.getScore();
+        memberScoreRepository.save(memberScore);
 
         // 4. Activity 저장
         activityRepository.save(existingActivity);
