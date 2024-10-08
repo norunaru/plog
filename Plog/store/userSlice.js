30a3379123ef6cd1fb3c2169660dcea12b3162e1
노우영 <nwy98@naver.com>
2024-10-08 12:29:38 +0900

    [FEAT] jwt에서 exp

--- Plog/store/userSlice.js
+++ Plog/store/userSlice.js
@@ -1,34 +1,36 @@
 import {jwtDecode} from 'jwt-decode';
 
-const createUserSlice = (set) => ({
-    id: null,
-    email: null,
-    nickname: null,
-    role: null,
-    isFirst: true,
-  
-    setUserFromToken: (accessToken) => {
-      const decoded = jwtDecode(accessToken);
-      set({
-        id: decoded.id,
-        email: decoded.email,
-        nickname: decoded.nickname,
-        role: decoded.role,
-        isFirst: decoded.isFirst,
-      });
-    },
-  
-    setIsFirst: (value) => set({ isFirst: value }),
-  
-    clearUser: async () => {
-      set({
-        id: null,
-        email: null,
-        nickname: null,
-        role: null,
-        isFirst: true,
-      });
-    },
-  });
-  
-  export default createUserSlice;
+const createUserSlice = set => ({
+  id: null,
+  email: null,
+  nickname: null,
+  role: null,
+  isFirst: true,
+  exp: 0,
+
+  setUserFromToken: accessToken => {
+    const decoded = jwtDecode(accessToken);
+    set({
+      id: decoded.id,
+      email: decoded.email,
+      nickname: decoded.nickname,
+      role: decoded.role,
+      isFirst: decoded.isFirst,
+      exp: decoded.exp,
+    });
+  },
+
+  setIsFirst: value => set({isFirst: value}),
+
+  clearUser: async () => {
+    set({
+      id: null,
+      email: null,
+      nickname: null,
+      role: null,
+      isFirst: true,
+    });
+  },
+});
+
+export default createUserSlice;
