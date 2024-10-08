f764eb8ee2adac25ce3f8026d0447e43e3aea6a0
조용훈 <sjsin0905@naver.com>
2024-10-08 09:43:20 +0900

    feat: 유저 검색 기능 정상화

--- Plog/src/main/java/com/plog/backend/domain/friend/controller/FriendController.java
+++ Plog/src/main/java/com/plog/backend/domain/friend/controller/FriendController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,7 +53,7 @@ public class FriendController {
         return SuccessResponse.deleted();
     }
 
-    @GetMapping("/search")
+    @PostMapping("/search")
     @Operation(summary = "유저 이메일 검색", description = "유저 이메일 검색 api")
     public SuccessResponse<?> getFriend(
         @RequestBody FriendFindByEmailRequestDto friendFindByEmailRequestDto) {
