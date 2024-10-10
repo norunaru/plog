4c779eed1a6b1cc1cbf07a0c1d852d32e90d3d81
gawon <gawon_127@naver.com>
2024-10-10 09:31:53 +0900

    [FIX] 일지 수정후 네비게이션

--- Plog/src/screens/WritingUpdateScreen.js
+++ Plog/src/screens/WritingUpdateScreen.js
@@ -84,7 +84,7 @@ const WritingUpdateScreen = ({ navigation, route }) => {
         accessToken
       );
   
-      navigation.navigate('PloggingRecordDetail', {activityId});
+      navigation.navigate('PloggingRecord');
     } catch (error) {
       console.error('저장 에러:', error);
     }
